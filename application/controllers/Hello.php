79e25130bcb9a0d3094b5031e47d405becbc2894
json <fxj@acewill.cn>
2022-10-05 12:05:34 +0800

    提供json响应封装方法

--- application/controllers/Hello.php
+++ application/controllers/Hello.php
@@ -10,6 +10,6 @@ class Hello extends MY_Controller {
 
     public function show() {
         Logger::error("错误","这是错误消息");
-        print_r($this->UserService->getUser());
+        $this->error('3','获取数据成功');
     }
 }
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -13,4 +13,23 @@ class MY_Controller extends CI_Controller {
     {
         parent::__construct();
     }
+
+    public function response($code,$data,$message) {
+        $response = [
+            'code'    =>  $code,
+            'data'    =>  $data,
+            'message' =>  $message
+        ];
+        $this->output
+            ->set_content_type('application/json', 'utf-8')
+            ->set_output(json_encode($response));
+    }
+
+    public function success($data,$message = '成功') {
+        $this->response(0,$data,$message);
+    }
+
+    public function error($code,$message='失败') {
+        $this->response((int)$code,[],$message);
+    }
 }
